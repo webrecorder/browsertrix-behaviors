9d14b6df5af15a781c8e206dbbc4415d1136f281
Ilya Kreymer <ikreymer@gmail.com>
2021-03-08 15:33:32 -0800

    license: add agplv3 license, webpack generate license banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: 'production',
-  plugins: [new webpack.ProgressPlugin()],
+  plugins: [
+    new webpack.BannerPlugin('behaviors.js is part of Webrecorder project. Copyright (C) 2021, Webrecorder Software. Licensed under the Affero General Public License v3.'),
+    new webpack.ProgressPlugin()
+  ],
   output: {
     iife: true,
     filename: 'behaviors.js'
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
 }
+
