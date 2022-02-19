eed751f2fbb5ba8de972016fa037e306a3bea45f
Ilya Kreymer <ikreymer@gmail.com>
2022-02-18 21:02:47 -0800

    autoscroll improvements: - experimental: skip autoscroll altogether if no scroll event listeners on window, document or body (using getEventListeners() debug function) - smoother scrolling - don't wait at end if only 1 segment - round canScrollCheck() to avoid possible infinite loop bump to 0.2.5

--- src/autoscroll.js
+++ src/autoscroll.js
@@ -18,31 +18,50 @@ export class AutoScroll extends Behavior
     return "Autoscroll";
   }
 
+  canScrollMore() {
+    return Math.round(self.scrollY + self.innerHeight) <
+    Math.max(
+      self.document.body.scrollHeight,
+      self.document.body.offsetHeight,
+      self.document.documentElement.clientHeight,
+      self.document.documentElement.scrollHeight,
+      self.document.documentElement.offsetHeight
+    );
+  }
+
+  hasScrollEL(obj) {
+    try {
+      return !!self.getEventListeners(obj).scroll;
+    } catch (e) {
+      // unknown, assume has listeners
+      this.debug("getEventListeners() not available");
+      return true;
+    }
+  }
+
   async* [Symbol.asyncIterator]() {
-    const canScrollMore = () =>
-      self.scrollY + self.innerHeight <
-      Math.max(
-        self.document.body.scrollHeight,
-        self.document.body.offsetHeight,
-        self.document.documentElement.clientHeight,
-        self.document.documentElement.scrollHeight,
-        self.document.documentElement.offsetHeight
-      );
+    const scrollInc = Math.min(self.document.body.clientHeight * 0.05, 30);
+    const interval = 75;
 
-    const scrollOpts = { top: 200, left: 0, behavior: "auto" };
-    const interval = waitUnit;
+    if (!this.hasScrollEL(self.window) &&
+        !this.hasScrollEL(self.document) &&
+        !this.hasScrollEL(self.document.body)) {
+      yield this.getState("Skipping autoscroll, page seems to be static (no 'scroll' event listeners)");
+      return;
+    }
 
-    //scrollOpts.top = Math.min(self.document.body.clientHeight * 0.01, 500);
+    const scrollOpts = { top: scrollInc, left: 0, behavior: "auto" };
 
     let showMoreElem = null;
-    let origHeight = self.document.body.clientHeight;
+    let lastScrollHeight = self.document.body.scrollHeight;
 
-    while (canScrollMore()) {
-      if (self.document.body.clientHeight > origHeight) {
+    while (this.canScrollMore()) {
+      const scrollHeight = self.document.body.scrollHeight;
+
+      if (scrollHeight > lastScrollHeight) {
         this.state.segments++;
+        lastScrollHeight = scrollHeight;
       }
-
-      origHeight = self.document.body.clientHeight;
 
       if (!showMoreElem) {
         showMoreElem = xpathNode(this.showMoreQuery);
@@ -55,7 +74,7 @@ export class AutoScroll extends Behavior
         await sleep(waitUnit);
 
         await Promise.race([
-          waitUntil(() => self.document.body.clientHeight > origHeight, 500),
+          waitUntil(() => self.document.body.scrollHeight > scrollHeight, 500),
           sleep(30000)
         ]);
 
@@ -67,12 +86,16 @@ export class AutoScroll extends Behavior
       yield this.getState(`Scrolling down by ${scrollOpts.top} pixels every ${interval / 1000.0} seconds`);
       
       await sleep(interval);
-      
-      // check for scrolling, but allow for more time for content to appear the longer have already scrolled
-      await Promise.race([
-        waitUntil(() => canScrollMore(), interval),
-        sleep(this.state.segments * 5000)
-      ]);
+
+      // only add extra wait if actually changed height
+      if (this.state.segments > 1) {
+        // check for scrolling, but allow for more time for content to appear the longer have already scrolled
+        await Promise.race([
+          waitUntil(() => this.canScrollMore(), interval),
+          sleep((this.state.segments - 1) * 2000)
+        ]);
+
+      }
     }
   }
 }
